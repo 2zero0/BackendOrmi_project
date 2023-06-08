9ff23687723a122d48cf75e4f9840e0b980329b6
young <cocoujy@naver.com>
2023-06-08 16:26:59 +0900

    loading.js의 style태그 최소화 (css로 이동)

--- project_1/js/loading.js
+++ project_1/js/loading.js
@@ -7,37 +7,21 @@ export function LoadingWithMask() {
 
   let mask = document.createElement("div");
   mask.id = "mask";
-  mask.style.position = "absolute";
-  mask.style.zIndex = "9000";
-  mask.style.backgroundColor = "#000000";
-  mask.style.display = "none";
-  mask.style.left = "0";
-  mask.style.top = "0";
+  mask.classList.add("loading-mask");
 
   let loadingImg = document.createElement("div");
   loadingImg.id = "loadingImg";
+
   let img = document.createElement("img");
   img.src = "img/cooking.gif";
-  img.style.position = "relative";
-  img.style.display = "block";
-  img.style.margin = "100px auto 20px";
-  img.style.width = "500px";
-  img.style.height = "500px";
+  img.classList.add("loading-img");
+
   loadingImg.appendChild(img);
 
   let message = document.createElement("p");
   message.textContent = "요리 구상중입니다 1분만 기다려주세요!";
-  message.style.textAlign = "center";
-  message.style.fontFamily = "NeoDunggeunmo";
-  message.style.fontSize = "large";
-  message.style.border = "5px solid white";
-  message.style.borderRadius = "20px";
-  message.style.color = "white";
-  message.style.backgroundColor = "#2eb7b7";
-  message.style.padding = "20px";
-  message.style.width = "600px";
-  message.style.height = "80px";
-  message.style.margin = "auto";
+  message.classList.add("loading-message");
+
   loadingImg.appendChild(message);
 
   document.body.appendChild(mask);
